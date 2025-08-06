4adb59097182f8f16e0fd442b008d3f3294426bd
saicharanbm <saicharanbm@gmail.com>
2025-08-07 00:04:46 +0530

    feat: add text type toggel feature

--- src/components/toolbar/TextToolBar.tsx
+++ src/components/toolbar/TextToolBar.tsx
@@ -3,12 +3,15 @@ import FontFamily from "./text-toolbar/FontFamily";
 import TextColor from "./text-toolbar/TextColor";
 import TextDecoration from "./text-toolbar/TextDecoration";
 import TextHighlighter from "./text-toolbar/TextHighlighter";
+import TextType from "./text-toolbar/TextType";
 import UndoRedo from "./text-toolbar/UndoRedo";
 
 function TextToolBar() {
   return (
     <div className="w-full flex items-center min-h-10 bg-[#F2EDF7] border-b-2 border-[#f0eaf7] p-1.5">
       <FontFamily />
+      <Divider />
+      <TextType />
       <Divider />
       <TextDecoration />
       <Divider />
---
+++ src/components/toolbar/text-toolbar/TextType.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
+import { useEditorStore } from "@/store/useEditorStore";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+
+type Level = 1 | 2 | 3 | 4 | 5 | 6;
+function TextType() {
+  const { editor } = useEditorStore();
+  const [open, setOpen] = useState(false); // local dropdown open state
+  const headings: { label: string; value: number; fontSize: string }[] = [
+    { label: "P", value: 0, fontSize: "16px" },
+    { label: "H1", value: 1, fontSize: "32px" },
+    { label: "H2", value: 2, fontSize: "24px" },
+    { label: "H3", value: 3, fontSize: "20px" },
+    { label: "H4", value: 4, fontSize: "18px" },
+    { label: "H5", value: 5, fontSize: "16px" },
+    { label: "H6", value: 6, fontSize: "14px" },
+  ];
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level <= 6; level++) {
+      if (editor?.isActive("heading", { level: level })) {
+        return `H${level}`;
+      }
+    }
+    return "P";
+  };
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">{getCurrentHeading()}</span>
+          {open ? (
+            <ChevronUpIcon className="ml-2 size-4 shrink-0" />
+          ) : (
+            <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+          )}
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              ((value === 0 && !editor?.isActive("heading")) ||
+                editor?.isActive("heading", { level: value })) &&
+                "bg-neutral-200/80"
+            )}
+            style={{ fontSize }}
+            onClick={() => {
+              if (value === 0) {
+                editor?.chain().focus().setParagraph().run();
+                return;
+              }
+              editor
+                ?.chain()
+                .focus()
+                .toggleHeading({ level: value as Level })
+                .run();
+            }}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default TextType;
